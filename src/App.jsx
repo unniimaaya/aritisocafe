c6f4926a6e0de66e78631fde41aed490c42c3817
unniimayacu <unnimaya@reontel.com>
2024-03-05 08:39:13 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Dashboard from './pages/Dashboard/Dashboard'
+
+function App() {
+
+  return (
+    <>
+    <Dashboard/>
+      
+    </>
+  )
+}
+
+export default App
